dfee2f4c29af28a4505473112735cc6216477356
Brandonalan70 <156138198+Brandonalan70@users.noreply.github.com>
2025-05-01 20:21:09 -0400

    Update standings_22-23.R

--- data visualization/standings_22-23.R
+++ data visualization/standings_22-23.R
@@ -1,12 +1,37 @@
 library(knitr)
 library(kableExtra)
 library(readr)
+library(rvest)
 
 #load file
-STANDINGS <- read_csv("laliga_standings.csv")
+#STANDINGS <- read_csv("laliga_standings.csv")
+
+url <- "https://www.espn.com/soccer/standings/_/league/ESP.1/season/2022"
+standings <- read_html(url) %>%
+  html_table() %>%
+  as.data.frame()
+
+la_liga_2023<- c(
+  "FC Barcelona", "Real Madrid CF", "Atlético de Madrid", "Real Sociedad",
+  "Villarreal CF", "Real Betis Balompié", "CA Osasuna", "Athletic Club de Bilbao",
+  "RCD Mallorca", "Girona FC", "Rayo Vallecano", "Sevilla FC", "RC Celta de Vigo",
+  "Cádiz CF", "Getafe CF", "Valencia CF", "Unión Deportiva Almería", "Real Valladolid CF",
+  "RCD Espanyol de Barcelona", "Elche CF"
+)
+
+standings <-
+  standings %>%
+  dplyr::select(1) %>%
+  rename(Club = 1) %>%
+
+#import club names that are universal throughout datasets
+  mutate(Club = la_liga_2023)%>%
+
+#add a column for rank
+  mutate(Rank = seq(1, nrow(standings)))
 
 #Create Table
-TABLE <- kable(STANDINGS, caption = "Final LaLiga Standings for 2022-23") %>%
+ standings <- kable(standings, caption = "Final LaLiga Standings for 2022-23") %>%
   kable_styling() %>%
   footnote(general =
              c("MP - Matches Played",
